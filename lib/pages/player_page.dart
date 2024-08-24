10b971a85ccd01fe5d49fd24794c1aa9413096af
Bruno <brunoalksouza@gmail.com>
2024-08-24 14:50:54 -0300

    FEAT Exibindo mensagem de erro caso não tenha arquivo de txt

--- lib/pages/player_page.dart
+++ lib/pages/player_page.dart
@@ -1,6 +1,8 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:midia_player/controllers/player_controller.dart';
+import 'package:midia_player/env.dart';
+import 'package:midia_player/functions/show_error_dialog.dart';
 import 'package:midia_player/widgets/image_widget.dart';
 import 'package:midia_player/widgets/video_widget.dart';
 
@@ -41,49 +43,71 @@ class _PlayerPageState extends State<PlayerPage> {
   void _abrirExecutavel() {
     if (executavelPath != null && executavelPath!.isNotEmpty) {
       try {
-        Process.run(executavelPath!, []);
+        Process.run(executavelPath!, []).then((result) {
+          if (result.exitCode != 0) {
+            ShowErrorDialog(
+              context,
+              'Erro: ${result.exitCode}\n'
+              'Saída do erro: ${result.stderr}',
+            );
+          }
+        }).catchError((e) {
+          ShowErrorDialog(
+            context,
+            'Erro ao abrir o caminho do executável, verifique se o caminho está correto:\n "$executavelPath"',
+          );
+        });
       } catch (e) {
-        print('Erro ao abrir o executável: $e');
+        ShowErrorDialog(
+          context,
+          'Erro ao abrir executável: $e',
+        );
       }
     } else {
-      print('Caminho do executável não encontrado.');
+      ShowErrorDialog(
+        context,
+        'Caminho do executável não encontrado. \n'
+        'Verifique seu arquivo de texto em "C:/mobi_player/caminho_executavel.txt", ele está vazio ou não existe.',
+      );
     }
   }
 
-  Widget _body() => ValueListenableBuilder<int>(
-        valueListenable: _controller.midiaNotifier,
-        builder: (context, value, child) {
-          if (_controller.midias.isEmpty) {
-            return const Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    'Nenhuma mídia encontrada',
-                    style: TextStyle(color: Colors.black),
-                  ),
-                  Text(
-                    'Adicione sua mídia em "C:/mobi_player"',
-                    style: TextStyle(color: Colors.black),
-                  ),
-                ],
-              ),
-            );
-          }
-          final midiaAtual = _controller.midias[_controller.indiceAtual];
-
-          return Center(
-            child: midiaAtual.path.endsWith('.mp4')
-                ? VideoWidget(
-                    videoController: _controller.videoController!,
-                    playerController: _controller,
-                  )
-                : ImageWidget(
-                    imagePath: midiaAtual.path,
-                  ),
+  Widget _body() {
+    return ValueListenableBuilder<int>(
+      valueListenable: _controller.midiaNotifier,
+      builder: (context, value, child) {
+        if (_controller.midias.isEmpty) {
+          return const Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Nenhuma mídia encontrada',
+                  style: TextStyle(color: Colors.black),
+                ),
+                Text(
+                  'Adicione sua mídia em "C:/mobi_player"',
+                  style: TextStyle(color: Colors.black),
+                ),
+              ],
+            ),
           );
-        },
-      );
+        }
+        final midiaAtual = _controller.midias[_controller.indiceAtual];
+
+        return Center(
+          child: midiaAtual.path.endsWith('.mp4')
+              ? VideoWidget(
+                  videoController: _controller.videoController!,
+                  playerController: _controller,
+                )
+              : ImageWidget(
+                  imagePath: midiaAtual.path,
+                ),
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
